7aa2fe2529a4576b4593c22668b86f7c9aa023d9
Denis Hoctor <denishoctor@gmail.com>
2010-07-06 20:56:06 +0100

    Adding in new files that weren't included in last checkin ( I always do this :S )

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BlackoutRugbyPOC.Controllers;
+
+namespace BlackoutRugby.API.POC2.Controllers {
+    [HandleError]
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            return View();
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Index(string requestParams, string devKey, string devIV, string devID) {
+            ViewData["ApiRequest"] = "http://api.blackoutrugby.com/?d=" + devID + "&dk=" + devKey + requestParams;
+            ViewData["ApiResponse"] = BlackoutRugbyAPI.Request(requestParams, devKey, devIV, devID);
+
+            return View("Show");
+        }
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,5 @@
+This is currently a simple Proof of Concept App for using the Blackout Rugby API. 
+
+I'm keen for anyone who has an interest in Blackout Rugby and/or HTML5 and all that has come to encompass that term to get involved! 
+
+Currently it's built on ASP.Net 4.0 MVC2. I plan to use Google's App Engine to create a service to write to Big Table for storage.
